f3f6434804be3e4f5842c0afd30082f2c5ce7a7e
Rivit Not Vivit <jack.problems@protonmail.com>
2026-01-26 05:58:05 -0800

    Added in vitetest and playwright testing framwork to be used for testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    globals: true,
+  },
   plugins: [
     react(),
     (() => {
